0edd7681d39882aca65d85dafa6eeccbd14208cf
bradger68 <bradger68@users.noreply.github.com>
2019-05-07 17:38:05 -0400

    Update hangman.py
    Made the guess-getter work better, made a function to choose secret word

--- hangman.py
+++ hangman.py
@@ -64,6 +64,69 @@ hangman_pics = [
 ]
 
 
+# Creates a list of words for the computer to choose from.
+words = 'alphabet bear crab drinks entire fringe gild hippo indecisive'.split()
+
+# Makes computer choose a word as the secret word.
+def secret_word(word_list):
+    wordIndex = random.randint(0, len(word_list) - 1)
+    return word_list[wordIndex]
+
+# Records users' valid guesses so far.
+guessed_letters = []
+
+# Gets the user to guess a letter.
+def get_guess():
+    while True:
+        guess = input(str(("Guess a letter: ")))
+        guess = guess.lower()
+        if len(guess) > 1:
+            print("Just one letter please.")
+        elif guess in guessed_letters:
+            print("You already guessed " + guess)
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print("A letter please!")
+        else:
+            guessed_letters.append(guess)
+            return guess
+
+# Function that displays the current Hangman situation as well as
+# guessed letters and secret word with blanks.
+
+/|     |
+       |
+ ______|
+ """,
+
+
+ """
+  |-----|
+  O     |
+ /|\    |
+        |
+  ______|
+  """,
+
+
+ """
+  |-----|
+  O     |
+ /|\    |
+ /      |
+  ______|
+  """,
+
+
+"""
+ |-----|
+ O     |
+/|\    |
+/ \    |
+ ______|
+ """
+]
+
+
 
 words = 'alphabet bear crab drinks entire fringe gild hippo indecisive'.split()
 
